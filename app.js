33dd184437c1bda6c3528598c5841e41685cd4b1
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-06-22 20:53:57 +0300

    adding not found Route Error

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const creatError = require("http-errors");
 const dotenv = require("dotenv");
+const { create } = require("underscore");
 
 dotenv.config();
 
@@ -9,6 +10,24 @@ const app = express();
 
 app.get("/", async (req, res, next) => {
   res.send("Hello From Express");
+});
+
+app.all("*", async (req, res, next) => {
+  // const error = new Error("Not Found");
+  // error.status = 404;
+  // next(error);
+  // next(creatError.NotFound("This route does not exist"));
+  next(creatError.NotFound());
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
 });
 
 const PORT = process.env.PORT || 3000;
